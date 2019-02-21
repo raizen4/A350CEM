8bb6ecf1b1947be2317d67041715c357fbf214d7
VladAndreescu <andreesv@uni.coventry.ac.uk>
2019-02-21 14:36:27 +0000

    finished implementing facade

--- Client/Client/Client/Services/Facade.cs
+++ Client/Client/Client/Services/Facade.cs
@@ -93,42 +93,295 @@ namespace Client.Services
             responseData.HasBeenSuccessful = false;
             responseData.Error = "Internal Error" + result.StatusCode.ToString();
             return responseData;
+        }
+
+        public async Task<ResponseData<IEnumerable<Aircraft>>> GetAircrafts()
+        {
+            var responseData = new ResponseData<IEnumerable<Aircraft>()
+            {
+                HasBeenSuccessful = false
+            };
+            var result = await this.apiWrapper.GetAircrafts();
+            string content = await result.Content.ReadAsStringAsync();
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    var deserializedContent = JsonConvert.DeserializeObject<ResponseData<IEnumerable<Aircraft>>>(content);
+
+                    if (!deserializedContent.HasBeenSuccessful || !deserializedContent.Content.Any())
+                    {
+                        responseData.HasBeenSuccessful = false;
+                        responseData.Content = null;
+                        responseData.Error = "Internal Server Error";
+                        return responseData;
+                    }
+
+                    responseData.HasBeenSuccessful = true;
+                    responseData.Content = deserializedContent.Content;
+                    responseData.Error = null;
+                    return responseData;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    responseData.HasBeenSuccessful = false;
+                    responseData.Content = null;
+                    responseData.Error = "Deserialization Error";
+                    return responseData;
+                }
+            }
+            throw new NotImplementedException();
+            
+        }
+
+        public async Task<ResponseData<IEnumerable<Employee>>> GetEmployees()
+        {
+            var responseData = new ResponseData<IEnumerable<Employee>>()
+            {
+                HasBeenSuccessful = false
+            };
+            var result = await this.apiWrapper.GetEmployees();
+            string content = await result.Content.ReadAsStringAsync();
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    var deserializedContent = JsonConvert.DeserializeObject<ResponseData<IEnumerable<Employee>>>(content);
+
+                    if (!deserializedContent.HasBeenSuccessful || !deserializedContent.Content.Any())
+                    {
+                        responseData.HasBeenSuccessful = false;
+                        responseData.Content = null;
+                        responseData.Error = "Internal Server Error";
+                        return responseData;
+                    }
+
+                    responseData.HasBeenSuccessful = true;
+                    responseData.Content = deserializedContent.Content;
+                    responseData.Error = null;
+                    return responseData;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    responseData.HasBeenSuccessful = false;
+                    responseData.Content = null;
+                    responseData.Error = "Deserialization Error";
+                    return responseData;
+                }
+            }
+            
             throw new NotImplementedException();
         }
 
-        public Task<ResponseData<IEnumerable<Aircraft>>> GetAircrafts()
+        public async Task<ResponseData<IEnumerable<Team>>> GetMembers(string teamId)
         {
-            throw new NotImplementedException();
+            var responseData = new ResponseData<IEnumerable<Team>>()
+            {
+                HasBeenSuccessful = false
+            };
+            var result = await this.apiWrapper.GetMembers(teamId);
+            string content = await result.Content.ReadAsStringAsync();
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    var deserializedContent = JsonConvert.DeserializeObject<ResponseData<IEnumerable<Team>>>(content);
+
+                    if (!deserializedContent.HasBeenSuccessful || !deserializedContent.Content.Any())
+                    {
+                        responseData.HasBeenSuccessful = false;
+                        responseData.Content = null;
+                        responseData.Error = "Internal Server Error";
+                        return responseData;
+                    }
+
+                    responseData.HasBeenSuccessful = true;
+                    responseData.Content = deserializedContent.Content;
+                    responseData.Error = null;
+                    return responseData;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    responseData.HasBeenSuccessful = false;
+                    responseData.Content = null;
+                    responseData.Error = "Deserialization Error";
+                    return responseData;
+                }
+            }
+            
+            responseData.HasBeenSuccessful = false;
+            responseData.Content = null;
+            responseData.Error = "Internal server Error";
+            return responseData;
+            
         }
 
-        public Task<ResponseData<IEnumerable<Employee>>> GetEmployees()
+        public async Task<ResponseData<IEnumerable<ServiceTask>>> GetTasksForAircraft(string aircraftId)
         {
-            throw new NotImplementedException();
+            var responseData = new ResponseData<IEnumerable<ServiceTask>>()
+            {
+                HasBeenSuccessful = false
+            };
+            var result = await this.apiWrapper.GetTasksForAircraft(aircraftId);
+            string content = await result.Content.ReadAsStringAsync();
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    var deserializedContent = JsonConvert.DeserializeObject<ResponseData<IEnumerable<ServiceTask>>>(content);
+
+                    if (!deserializedContent.HasBeenSuccessful || !deserializedContent.Content.Any())
+                    {
+                        responseData.HasBeenSuccessful = false;
+                        responseData.Content = null;
+                        responseData.Error = "Internal Server Error";
+                        return responseData;
+                    }
+
+                    responseData.HasBeenSuccessful = true;
+                    responseData.Content = deserializedContent.Content;
+                    responseData.Error = null;
+                    return responseData;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    responseData.HasBeenSuccessful = false;
+                    responseData.Content = null;
+                    responseData.Error = "Deserialization Error";
+                    return responseData;
+                }
+            }
+
+            responseData.HasBeenSuccessful = false;
+            responseData.Content = null;
+            responseData.Error = "Internal server Error";
+            return responseData;
         }
 
-        public Task<ResponseData<IEnumerable<Team>>> GetMembers(string teamId)
+        public async Task<ResponseData<IEnumerable<Team>>> GetTeam(string teamId)
         {
-            throw new NotImplementedException();
+            var responseData = new ResponseData<IEnumerable<Team>>()
+            {
+                HasBeenSuccessful = false
+            };
+            var result = await this.apiWrapper.GetTeam(teamId);
+            string content = await result.Content.ReadAsStringAsync();
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    var deserializedContent = JsonConvert.DeserializeObject<ResponseData<IEnumerable<Team>>>(content);
+
+                    if (!deserializedContent.HasBeenSuccessful || !deserializedContent.Content.Any())
+                    {
+                        responseData.HasBeenSuccessful = false;
+                        responseData.Content = null;
+                        responseData.Error = "Internal Server Error";
+                        return responseData;
+                    }
+
+                    responseData.HasBeenSuccessful = true;
+                    responseData.Content = deserializedContent.Content;
+                    responseData.Error = null;
+                    return responseData;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    responseData.HasBeenSuccessful = false;
+                    responseData.Content = null;
+                    responseData.Error = "Deserialization Error";
+                    return responseData;
+                }
+            }
+
+            responseData.HasBeenSuccessful = false;
+            responseData.Content = null;
+            responseData.Error = "Internal server Error";
+            return responseData;
         }
 
-        public Task<ResponseData<IEnumerable<ServiceTask>>> GetTasksForAircraft(string aircraftId)
+        public async Task<ResponseData<IEnumerable<Team>>> GetTeams()
         {
-            throw new NotImplementedException();
+            var responseData = new ResponseData<IEnumerable<Team>>()
+            {
+                HasBeenSuccessful = false
+            };
+            var result = await this.apiWrapper.GetTeams();
+            string content = await result.Content.ReadAsStringAsync();
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    var deserializedContent = JsonConvert.DeserializeObject<ResponseData<IEnumerable<Team>>>(content);
+
+                    if (!deserializedContent.HasBeenSuccessful || !deserializedContent.Content.Any())
+                    {
+                        responseData.HasBeenSuccessful = false;
+                        responseData.Content = null;
+                        responseData.Error = "Internal Server Error";
+                        return responseData;
+                    }
+
+                    responseData.HasBeenSuccessful = true;
+                    responseData.Content = deserializedContent.Content;
+                    responseData.Error = null;
+                    return responseData;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    responseData.HasBeenSuccessful = false;
+                    responseData.Content = null;
+                    responseData.Error = "Deserialization Error";
+                    return responseData;
+                }
+            }
+
+            responseData.HasBeenSuccessful = false;
+            responseData.Content = null;
+            responseData.Error = "Internal server Error";
+            return responseData;
         }
 
-        public Task<ResponseData<IEnumerable<Team>>> GetTeam(string teamId)
+        public async Task<ResponseBase> Login(string password)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<ResponseData<IEnumerable<Team>>> GetTeams()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ResponseBase> Login(string password)
-        {
-            throw new NotImplementedException();
+            var responseData = new ResponseData<IEnumerable<Team>>()
+            {
+                HasBeenSuccessful = false
+            };
+            var result = await this.apiWrapper.Login(password);
+            string content = await result.Content.ReadAsStringAsync();
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    var deserializedContent = JsonConvert.DeserializeObject<ResponseBase>(content);
+                    if (!deserializedContent.HasBeenSuccessful || deserializedContent.Error != null)
+                    {
+                        responseData.HasBeenSuccessful = false;
+                        responseData.Error = "Internal Server Error";
+                        return responseData;
+                    }
+                    responseData.HasBeenSuccessful = true;
+                    responseData.Error = null;
+                    return responseData;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    responseData.HasBeenSuccessful = false;
+                    responseData.Error = "Deserialization Error";
+                    return responseData;
+                }
+            }
+            responseData.HasBeenSuccessful = false;
+            responseData.Error = "Internal Error" + result.StatusCode.ToString();
+            return responseData;
         }
     }
 }
