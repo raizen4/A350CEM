1f705418169171642929086eab624325d936ef9c
VladAndreescu <andreesv@uni.coventry.ac.uk>
2019-03-22 17:42:40 +0000

    implemented GetTeams and getTeamMembers in IDatabaseService

--- Api/Api/Interfaces/IDatabaseService.cs
+++ Api/Api/Interfaces/IDatabaseService.cs
@@ -13,13 +13,23 @@ namespace Api.Interfaces
         User GetUser(string password);
         User CreateUser(User user);
 
+<<<<<<< HEAD
         Team CreateTeam(Team team);
+=======
+        IEnumerable<Aircraft> GetAircrafts();
+        IEnumerable<Team> GetTeams();
+>>>>>>> implemented GetTeams and getTeamMembers in IDatabaseService
 
         Employee CreateEmployee(Employee employee);
         bool AssignEmployeeToTeam(string employeeId, string teamId);
         IEnumerable<Employee> GetEmployees();
 
+<<<<<<< HEAD
         Aircraft CreateAircraft(Aircraft aircraft);
+=======
+        IEnumerable<Client.Models.Task> GetTasksForAircraft(string aircraftId);
+        IEnumerable<Team> GetTeamMembers(string teamId);
+>>>>>>> implemented GetTeams and getTeamMembers in IDatabaseService
         bool MarkTaskAsCompleted(string taskId);
         IEnumerable<Aircraft> GetAircrafts();
         IEnumerable<Task> GetTasksForAircraft(string aircraftId);
